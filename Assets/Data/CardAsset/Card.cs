59e1604be4a275ca2496c130025b0055d0cab91a
A'sa Dickens <asadickens@gmail.com>
2024-07-22 00:31:27 -0400

    Added Deck Asset
    - updated Card to have its fields public to serialize them in the inspector
    - added Deck
    - added SampleCard and SampleDeck as examples
    - saved unity so other assets that were dirty were saved

--- Assets/Data/CardAsset/Card.cs
+++ Assets/Data/CardAsset/Card.cs
@@ -8,19 +8,20 @@ namespace ShadowCraft
     public class Card: ScriptableObject
     {
         [Serializable]
-        class ManaCost
+        public class ManaCost
         {
             public Mana.Type manaType = Mana.Type.Void;
             public int cost = 1;
         }
 
-        int attack = 1;
-        int health = 1;
+        public int attack = 1;
+        public int health = 1;
 
         [TextArea]
-        string description = "This card does nothing!";
+        public string description = "This card does nothing!";
 
-        List<ManaCost> manaCosts = new List<ManaCost>();
+        [SerializeField]
+        public List<ManaCost> manaCosts = new List<ManaCost>();
 
         // TODO: Figure out the Board Passives
         // TODO: Figure out the dark/light augmentations
