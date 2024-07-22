a4503a3e999e83037bbb72a3c57eb8638c181b04
A'sa Dickens <asadickens@gmail.com>
2024-07-22 00:23:10 -0400

    Added Card Asset
    - added Card scriptable object to mimic a card
    - added a Card Tag
    - added CardWidget prefab to represent the visual display of the Card and assigned the Card tag

---
+++ Assets/Data/ManaAsset/Mana.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mana : ScriptableObject
+{
+    public enum Type
+    {
+        Fire, Water, Air, Grass, Rock, Void
+    }
+}
---
+++ Assets/Scripts/Prefabs/CardWidget.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ShadowCraft
+{
+    public class CardWidget : MonoBehaviour
+    {
+        Card card { get; set; }
+    }
+}
