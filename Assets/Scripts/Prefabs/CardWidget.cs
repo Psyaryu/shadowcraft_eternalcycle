da81b446c100dc1b8d5001a89a52ab0e249aa186
A'sa Dickens <asadickens@gmail.com>
2024-07-23 18:22:59 -0400

    Added InputManager
    - added InputManager class to work with CardWidgets to support basic functionality
    - updated CardWidget to print all the callbacks
    - updated CardWidget.prefab to have the Card tag and the collider in the right areas

--- Assets/Scripts/Prefabs/CardWidget.cs
+++ Assets/Scripts/Prefabs/CardWidget.cs
@@ -5,5 +5,30 @@ namespace ShadowCraft
     public class CardWidget : MonoBehaviour
     {
         Card card { get; set; }
+
+        public void OnHoverEnter()
+        {
+            Debug.Log("On Hover");
+        }
+
+        public void OnHoverExit()
+        {
+            Debug.Log("On Stop Hover");
+        }
+
+        public void OnClick()
+        {
+            Debug.Log("On Click");
+        }
+
+        public void OnClickRelease()
+        {
+            Debug.Log("On Click Release");
+        }
+
+        public void OnDrag(Vector2 newLocation)
+        {
+            Debug.Log("On Drag");
+        }
     }
 }
