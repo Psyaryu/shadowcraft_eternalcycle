d7bb7f516cdbe57c400d1a77f1ed4033c9c35f46
A'sa Dickens <asadickens@gmail.com>
2024-07-26 19:16:44 -0400

    story/86b1dvcbj-create-board-slot-prefab (#12)
    - added BoardSlotPrefab

---
+++ Assets/Scripts/Prefabs/BoardSlot.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShadowCraft
+{
+    public class BoardSlot : MonoBehaviour
+    {
+
+        enum CycleType
+        {
+            Light,
+            Shadow
+        }
+
+        public int SlotNumber = 0;
+
+        [SerializeField]
+        private MeshRenderer meshRenderer = null;
+
+        [SerializeField]
+        private Material material = null;
+
+        [SerializeField]
+        private Color DarkColor = Color.black;
+
+        [SerializeField]
+        private Color LightColor = Color.white;
+
+        [SerializeField]
+        private Color HoverLightColor = Color.green;
+
+        [SerializeField]
+        private Color HoverDarkColor = Color.green;
+
+        [SerializeField]
+        private Color HoverFilledLightColor = Color.green;
+
+        [SerializeField]
+        private Color HoverFilledDarkColor = Color.green;
+
+        private CycleType cycleType = CycleType.Light;
+
+        private Card card = null;
+
+        public void SetCard(Card card) => this.card = card;
+
+        private void Awake()
+        {
+            meshRenderer.material = Instantiate(material);
+        }
+
+        public void OnDark()
+        {
+            meshRenderer.material.color = DarkColor;
+            cycleType = CycleType.Shadow;
+        }
+
+        public void OnLight()
+        {
+            meshRenderer.material.color = LightColor;
+            cycleType = CycleType.Light;
+        }
+
+        public void OnHover()
+        {
+            Debug.Log($"Board Slot Hover: {SlotNumber}");
+
+            if (card == null)
+                meshRenderer.material.color = cycleType == CycleType.Light ? HoverLightColor : HoverDarkColor;
+            else
+                meshRenderer.material.color = cycleType == CycleType.Light ? HoverFilledLightColor : HoverFilledDarkColor;
+        }
+
+        public void OnHoverExit()
+        {
+            Debug.Log($"Board Slot Hover Exit: {SlotNumber}");
+
+            meshRenderer.material.color = cycleType == CycleType.Light ? LightColor : DarkColor;
+        }
+    }
+}
--- Assets/Scripts/Prefabs/GameBoardWidget.cs
+++ Assets/Scripts/Prefabs/GameBoardWidget.cs
@@ -14,7 +14,7 @@ namespace ShadowCraft
         int numberOfCardSlots = 5;
 
         [SerializeField]
-        List<Transform> CardSlots = new List<Transform>();
+        List<BoardSlot> CardSlots = new List<BoardSlot>();
 
         [SerializeField]
         List<Transform> DeckPositions = new List<Transform>();
@@ -27,20 +27,28 @@ namespace ShadowCraft
         private void Awake()
         {
             cards = new CardWidget[CardSlots.Count];
+            for (int i = 0; i < CardSlots.Count; i++)
+                CardSlots[i].SlotNumber = i;
         }
+
+        //private void Start()
+        //{
+        //    for (int i = 0; i < CardSlots.Count; i++)
+        //        CardSlots[i].SlotNumber = i;
+        //}
 
         public void AddCard(CardWidget cardWidget, int slot)
         {
             if (slot >= CardSlots.Count)
                 return;
 
-            cardWidget.transform.position = CardSlots[slot].position;
+            cardWidget.transform.position = CardSlots[slot].transform.position;
             cards[slot] = cardWidget;
         }
 
         public void MoveCard(int fromSlot, int toSlot)
         {
-            cards[fromSlot].transform.position = CardSlots[toSlot].position;
+            cards[fromSlot].transform.position = CardSlots[toSlot].transform.position;
             cards[toSlot] = cards[fromSlot];
             cards[fromSlot] = null;
         }
@@ -66,14 +74,14 @@ namespace ShadowCraft
             {
                 var positionX = (startX * (i % numberOfCardSlots)) - (width / 2 - width / numberOfCardSlots / 2);
                 var positionY = (playerY + (height / 2f * (int)(i / numberOfCardSlots)));
-                CardSlots[i].position = new Vector3(positionX, positionY, CardSlots[i].position.z);
+                CardSlots[i].transform.position = new Vector3(positionX, positionY, CardSlots[i].transform.position.z);
             }
         }
 
         private void OnDrawGizmos()
         {
             Handles.color = Color.red;
-            CardSlots.ForEach(Slot => Handles.DrawSolidArc(Slot.position, Slot.forward, Slot.up, 360f, 1f));
+            CardSlots.ForEach(Slot => Handles.DrawSolidArc(Slot.transform.position, Slot.transform.forward, Slot.transform.up, 360f, 1f));
 
             Handles.color = Color.blue;
             DeckPositions.ForEach(Slot => Handles.DrawSolidArc(Slot.position, Slot.forward, Slot.up, 360f, 1f));
